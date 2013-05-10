85d56918925fe673c1dece146fbeda3ee94ae8a5
dstrauss <dstrauss@ds-2.de>
2013-05-10 09:50:45 +0200

    OSSCORE-3: add missing files Signed-off-by: dstrauss <dstrauss@ds-2.de>

---
+++ es/src/main/java/ds2/oss/core/elasticsearch/api/IndexTypes.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ds2.oss.core.elasticsearch.api;
+
+/**
+ *
+ * @author dstrauss
+ */
+public enum IndexTypes {
+  NO,NOT_ANALYZED;
+}
