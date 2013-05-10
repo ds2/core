a45d4253743947f583c51c2a2971b30261d03e4d
dstrauss <dstrauss@ds-2.de>
2013-05-10 09:46:54 +0200

    OSSCORE-3: add initial idea Signed-off-by: dstrauss <dstrauss@ds-2.de>

---
+++ es/src/main/java/ds2/oss/core/elasticsearch/ap/CreateTypeMappingsAP.java
@@ -0,0 +1,8 @@
+package ds2.oss.core.elasticsearch.ap;
+
+/**
+ * The type mapping annotation processor.
+ * @author dstrauss
+ */
+public class CreateTypeMappingsAP {
+}
---
+++ es/src/main/java/ds2/oss/core/elasticsearch/api/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * The API contracts.
+ */
+package ds2.oss.core.elasticsearch.api;
