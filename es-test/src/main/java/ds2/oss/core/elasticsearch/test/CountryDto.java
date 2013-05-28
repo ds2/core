008bce59d7518110ec798793db12b2d5c5a4d2e1
dstrauss <dstrauss@ds-2.de>
2013-05-28 14:32:12 +0200

    OSSCORE-3: add country
    Signed-off-by: dstrauss <dstrauss@ds-2.de>

---
+++ es-test/src/main/java/ds2/oss/core/elasticsearch/test/CountryDto.java
@@ -0,0 +1,53 @@
+/**
+ *
+ */
+package ds2.oss.core.elasticsearch.test;
+
+import ds2.oss.core.elasticsearch.api.PropertyMapping;
+import ds2.oss.core.elasticsearch.api.TypeMapping;
+
+/**
+ * @author  dstrauss
+ */
+@TypeMapping(value = "country")
+public class CountryDto {
+
+    @PropertyMapping
+    private String name;
+    @PropertyMapping
+    private String isoCode;
+
+    /**
+     */
+    public CountryDto() {
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @return  the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param  name  the name to set
+     */
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return  the isoCode
+     */
+    public String getIsoCode() {
+        return isoCode;
+    }
+
+    /**
+     * @param  isoCode  the isoCode to set
+     */
+    public void setIsoCode(final String isoCode) {
+        this.isoCode = isoCode;
+    }
+}
