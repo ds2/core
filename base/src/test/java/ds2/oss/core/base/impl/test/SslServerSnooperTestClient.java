702d95256fe38a13cf6985d3cf1b760f2fcd441c
dstrauss <dstrauss@ds-2.de>
2013-07-15 15:37:10 +0200

    OSSCORE-0: license format
    Signed-off-by: dstrauss <dstrauss@ds-2.de>

--- base/src/test/java/ds2/oss/core/base/impl/test/SslServerSnooperTestClient.java
+++ base/src/test/java/ds2/oss/core/base/impl/test/SslServerSnooperTestClient.java
@@ -1,5 +1,17 @@
-/**
- * 
+/*
+ * Copyright 2012-2013 Dirk Strauss
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package ds2.oss.core.base.impl.test;
 
@@ -32,7 +44,7 @@ public final class SslServerSnooperTestClient {
         WeldWrapper.onSuiteStart();
         final SslServerSnooper to =
             WeldWrapper.getInstance(SslServerSnooper.class);
-        String hostname = args.length > 0 ? args[0] : "localhost";
+        final String hostname = args.length > 0 ? args[0] : "localhost";
         final X509Certificate[] certs = to.getServerCertificates(hostname, 443);
         System.out.println(certs);
         WeldWrapper.afterSuite();
