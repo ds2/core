12905e0f2e99c57c711d4f3985f29ff25757972c
dstrauss <dstrauss@ds-2.de>
2013-08-08 20:23:07 +0200

    OSSCORE-4 first javadoc
    Signed-off-by: dstrauss <dstrauss@ds-2.de>

--- api/src/main/java/ds2/oss/core/api/InfinispanService.java
+++ api/src/main/java/ds2/oss/core/api/InfinispanService.java
@@ -2,7 +2,19 @@ package ds2.oss.core.api;
 
 /**
  * The infinispan service.
+ * 
+ * @version 0.3
+ * @author dstrauss
  */
 public interface InfinispanService {
+    /**
+     * Stores an item in the cluster.
+     * 
+     * @param e
+     *            the item to store
+     * @param <E>
+     *            the type of the item
+     * @return the item again
+     */
     <E> E store(E e);
 }
