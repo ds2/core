a58a6f581baacd0d3ec8b9a5875c36baa2f43c8f
dstrauss <dstrauss@ds-2.de>
2013-08-21 13:32:46 +0200

    OSSCORE-13 add version contract

---
+++ api/src/main/java/ds2/oss/core/api/Version.java
@@ -0,0 +1,12 @@
+package ds2.oss.core.api;
+
+import java.io.Serializable;
+
+/**
+ * A version.
+ * @author dstrauss
+ * @version 0.3
+ */
+public interface Version extends Serializable, Comparable<Version>{
+  //no special methods needed.
+}
