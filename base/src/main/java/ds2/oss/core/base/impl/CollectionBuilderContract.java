f5ed17df7d2a42208839ec2252b3a73dbe486363
dstrauss <dstrauss@ds-2.de>
2013-12-15 14:36:34 +0100

    OSSCORE-0: more ideas for the CB

--- base/src/main/java/ds2/oss/core/base/impl/CollectionBuilderContract.java
+++ base/src/main/java/ds2/oss/core/base/impl/CollectionBuilderContract.java
@@ -3,12 +3,58 @@ package ds2.oss.core.base.impl;
 import java.util.Collection;
 
 /**
- * Created by dstrauss on 04.10.13.
+ * Contract for the collection builder.
+ * 
+ * @author dstrauss
+ * @version 0.3
+ * @param <E>
+ *            the collection type
+ * @param <Z>
+ *            the item type
  */
-public interface CollectionBuilderContract<E extends Collection<Z>,Z> {
-  CollectionBuilderContract<E,Z> allowNull(boolean b);
-  CollectionBuilderContract<E,Z> add(Z z);
-  CollectionBuilderContract<E,Z> addAt(int index, Z z);
-  CollectionBuilderContract<E,Z> remove(Z z);
-  E build();
+public interface CollectionBuilderContract<E extends Collection<Z>, Z> {
+    /**
+     * Whether to allow adding null values.
+     * 
+     * @param b
+     *            the flag value
+     * @return this instance
+     */
+    CollectionBuilderContract<E, Z> allowNull(boolean b);
+    
+    /**
+     * Adds an item to the collection.
+     * 
+     * @param z
+     *            the item
+     * @return this instance
+     */
+    CollectionBuilderContract<E, Z> add(Z z);
+    
+    /**
+     * Adds an item at a specific position.
+     * 
+     * @param index
+     *            the position
+     * @param z
+     *            the item
+     * @return this instance
+     */
+    CollectionBuilderContract<E, Z> addAt(int index, Z z);
+    
+    /**
+     * Removes an item.
+     * 
+     * @param z
+     *            the item to remove
+     * @return this instance
+     */
+    CollectionBuilderContract<E, Z> remove(Z z);
+    
+    /**
+     * Creates the collection.
+     * 
+     * @return the collection
+     */
+    E build();
 }
