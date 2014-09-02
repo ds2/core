772699d3ab06afbcf8cdcc3ca578466102a8b451
dstrauss <dstrauss@ds-2.de>
2014-09-02 18:59:39 +0200

    - license format

--- options/src/main/java/ds2/oss/core/options/api/OptionValueEncrypterProvider.java
+++ options/src/main/java/ds2/oss/core/options/api/OptionValueEncrypterProvider.java
@@ -1,5 +1,17 @@
-/**
- * 
+/*
+ * Copyright 2012-2014 Dirk Strauss
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package ds2.oss.core.options.api;
 
@@ -13,5 +25,16 @@ import ds2.oss.core.api.options.ValueType;
  *
  */
 public interface OptionValueEncrypterProvider {
+    /**
+     * Returns the value encrypter for the given value type.
+     * 
+     * @param t
+     *            the value type
+     * @param v
+     *            the target type of the encrypter
+     * @return the found encrypter, or null if not found
+     * @param <V>
+     *            the target type
+     */
     <V> OptionValueEncrypter<V> getForValueType(ValueType t, Class<V> v);
 }
