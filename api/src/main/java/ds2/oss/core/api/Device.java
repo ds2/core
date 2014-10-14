2989ae02eeaec6f0dfddab85bc0c44f65327b405
Dirk Strauss <dstrauss@ds-2.de>
2014-10-14 16:02:42 +0200

    - add idea for Device revision

--- api/src/main/java/ds2/oss/core/api/Device.java
+++ api/src/main/java/ds2/oss/core/api/Device.java
@@ -1,22 +1,30 @@
-/**
- * 
- */
 package ds2.oss.core.api;
 
 /**
  * Any hardware device.
+ * 
  * @author dstrauss
  * @version 0.3
  */
 public interface Device {
-	/**
-	 * Returns the vendor of the device.
-	 * @return the device's vendor
-	 */
-	String getVendor();
-	/**
-	 * Returns the model name of this device.
-	 * @return the model name
-	 */
-	String getModel();
+    /**
+     * Returns the vendor of the device.
+     * 
+     * @return the device's vendor
+     */
+    String getVendor();
+    
+    /**
+     * Returns the model name of this device.
+     * 
+     * @return the model name
+     */
+    String getModel();
+    
+    /**
+     * REturns the revision of this device.
+     * 
+     * @return the revision.
+     */
+    String getRevision();
 }
