0e0b1ef38234f075acbd4122060e7c26f237fc59
Dirk Strauss <dstrauss@ds-2.de>
2016-05-20 12:21:10 +0200

    - add a dummy static method to create a file

---
+++ statics/src/main/java/ds2/oss/core/statics/IoMethods.java
@@ -0,0 +1,51 @@
+package ds2.oss.core.statics;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.lang.invoke.MethodHandles;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
+/**
+ * Created by dstrauss on 19.05.16.
+ */
+public interface IoMethods {
+    /**
+     * A logger.
+     */
+    Logger LOG= LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    /**
+     * Recursively deletes a given path directory.
+     * @param directory the directory to delete
+     * @throws IOException if an IO error occurred
+     */
+    static void deleteTree(Path directory) throws IOException {
+        if (Files.exists(directory)) {
+            Files.walkFileTree(directory, new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    LOG.debug("Deleting {}", file);
+                    Files.delete(file);
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                    LOG.debug("Deleting directory {}", dir);
+                    Files.delete(dir);
+                    return FileVisitResult.CONTINUE;
+                }
+
+            });
+        }
+    }
+
+    static void touchFile(Path file) throws IOException {
+        Path dir=file.getParent();
+        Files.createDirectories(dir);
+        Files.write(file, "".getBytes("utf-8"), StandardOpenOption.CREATE_NEW,StandardOpenOption.WRITE);
+    }
+}
