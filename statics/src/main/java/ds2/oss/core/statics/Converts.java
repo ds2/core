617a59c1df2bb5015ca3c90c60ca64c05dbffca6
Dirk Strauss <dstrauss@ds-2.de>
2016-06-07 15:59:30 +0200

    - use new Converts interface

--- statics/src/main/java/ds2/oss/core/statics/Converts.java
+++ statics/src/main/java/ds2/oss/core/statics/Converts.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2012-2015 Dirk Strauss
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package ds2.oss.core.statics;
 
 import org.slf4j.Logger;
@@ -23,33 +8,24 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 /**
- * @author dstrauss
- * @deprecated Better use {@link Converts} instead.
+ * Contract to convert somethings.
  */
-@Deprecated
-public final class Convert {
+public interface Converts {
     /**
      * A logger.
      */
-    private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
-    
-    /**
-     * Inits this object.
-     */
-    private Convert() {
-        // nothing special to do
-    }
+    Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
     /**
      * Converts an object into an int.
-     * 
+     *
      * @param o
      *            the object
      * @param defValue
      *            a default value
      * @return the converted value, or the default value
      */
-    public static int toInt(Object o, int defValue) {
+    static int toInt(Object o, int defValue) {
         int rc = defValue;
         if (o != null) {
             if (o instanceof Number) {
@@ -65,17 +41,17 @@ public final class Convert {
         }
         return rc;
     }
-    
+
     /**
      * Converts a given object into long.
-     * 
+     *
      * @param o
      *            the object
      * @param defValue
      *            the default value
      * @return the long value
      */
-    public static long toLong(Object o, long defValue) {
+    static long toLong(Object o, long defValue) {
         long rc = defValue;
         if (o != null) {
             if (o instanceof Number) {
@@ -99,7 +75,7 @@ public final class Convert {
      *            the url string
      * @return the url object, or null if an error occurred
      */
-    public static URL toUrl(final String urlStr) {
+    static URL toUrl(final String urlStr) {
         URL rc = null;
         try {
             rc = new URL(urlStr);
@@ -108,5 +84,4 @@ public final class Convert {
         }
         return rc;
     }
-    
 }
