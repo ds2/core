7f2e8c2f9492615758fe5ca2392cd0874957d108
Dirk Strauss <dstrauss@ds-2.de>
2016-06-10 07:37:27 +0200

    - dummy testcase

---
+++ statics/src/test/java/ds2/oss/core/statics/MethodsTest.java
@@ -0,0 +1,28 @@
+package ds2.oss.core.statics;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import javax.management.remote.JMXServiceURL;
+
+/**
+ * Created by dstrauss on 08.06.16.
+ */
+public class MethodsTest {
+    @Test
+    public void testNumber1(){
+        Assert.assertNull(Methods.parseNumber(null));
+    }
+    @Test
+    public void testNumber2(){
+        Number n=Methods.parseNumber("23");
+        Assert.assertNotNull(n);
+        Assert.assertEquals(n.intValue(),23);
+    }
+
+    @Test(enabled = false)
+    public void testVms(){
+        JMXServiceURL url=Methods.getJmxServiceUrlByLocalPid(1235);
+        Assert.assertNotNull(url);
+    }
+}
