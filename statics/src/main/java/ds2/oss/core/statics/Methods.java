05ef2a1f9e33b1b60f9d83c4748d35f46b63a164
Dirk Strauss <dstrauss@ds-2.de>
2016-06-10 07:36:27 +0200

    - add new method

--- statics/src/main/java/ds2/oss/core/statics/Methods.java
+++ statics/src/main/java/ds2/oss/core/statics/Methods.java
@@ -15,12 +15,45 @@
  */
 package ds2.oss.core.statics;
 
+import com.sun.tools.attach.AttachNotSupportedException;
+import com.sun.tools.attach.VirtualMachine;
+import com.sun.tools.attach.VirtualMachineDescriptor;
+import com.sun.tools.javac.jvm.Pool;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import sun.management.ConnectorAddressLink;
+
+import javax.management.remote.JMXServiceURL;
+import java.io.IOException;
+import java.lang.invoke.MethodHandles;
+import java.net.MalformedURLException;
 import java.util.Collection;
+import java.util.List;
 
 /**
  * Created by deindesign on 21.12.15.
  */
 public interface Methods {
+    Logger LOG= LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    static Number parseNumber(String s){
+        Number rc=null;
+        if(!isBlank(s)){
+            try {
+                Double d = Double.valueOf(s);
+                if(d.longValue()==d){
+                    rc=Long.valueOf(s);
+                } else if(d.intValue()==d){
+                    rc=Integer.valueOf(s);
+                } else {
+                    rc = d;
+                }
+            } catch(NumberFormatException e){
+                //silently ignored
+            }
+        }
+        return rc;
+    }
     /**
      * Returns the first element from a given collection, or the default value.
      * @param c the collection
@@ -90,8 +123,52 @@ public interface Methods {
      * Checks if the given string contains some value.
      * @param s the string to check
      * @return TRUE if not blank, otherwise FALSE.
+     * @deprecated Better use {@link #isBlank(String)} instead.
      */
+    @Deprecated
     static boolean isNotBlank(String s){
         return s!=null&&s.trim().length()>0;
+    }
+    /**
+     * Checks if the given string contains some value.
+     * @param s the string to check
+     * @return TRUE if not blank, otherwise FALSE.
+     */
+    static boolean isBlank(String s){
+        return s==null||s.trim().length()<=0;
+    }
+
+    static JMXServiceURL getJmxServiceUrlByLocalPid(int pid){
+        JMXServiceURL rc=null;
+        List<VirtualMachineDescriptor> vms=VirtualMachine.list();
+        for(VirtualMachineDescriptor vmd : vms){
+            VirtualMachine vm=null;
+            try {
+                vm=VirtualMachine.attach(vmd);
+                //VirtualMachine vm2=VirtualMachine.attach(""+pid);
+            } catch (AttachNotSupportedException e) {
+                LOG.debug("Cannot attach to this VM!", e);
+            } catch (IOException e) {
+                LOG.debug("IO error when trying to attach!", e);
+            }
+            if(vm==null){
+                continue;
+            }
+            String connectorProp;
+            try {
+                LOG.debug("VMs properties: {}", vm.getSystemProperties());
+                connectorProp = vm.getSystemProperties().getProperty("com.sun.management.jmxremote.localConnectorAddress");
+                if(isBlank(connectorProp)){
+                    connectorProp=ConnectorAddressLink.importFrom(Converts.toInt(vmd.id().trim(),0));
+                }
+                if(!isBlank(connectorProp)){
+                    LOG.debug("Found url: {}", connectorProp);
+                    rc=new JMXServiceURL(connectorProp);
+                }
+            } catch (IOException e) {
+                LOG.debug("IO error when using the connection!");
+            }
+        }
+        return rc;
     }
 }
