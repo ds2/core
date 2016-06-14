6837fe1a7e1a9c43abf4082fe1c5ebf9c7849c0d
Dirk Strauss <dstrauss@ds-2.de>
2016-06-14 08:04:27 +0200

    - code formatting

--- statics/src/main/java/ds2/oss/core/statics/Methods.java
+++ statics/src/main/java/ds2/oss/core/statics/Methods.java
@@ -18,75 +18,80 @@ package ds2.oss.core.statics;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.lang.invoke.MethodHandles;
 import java.util.Collection;
 
 /**
  * Created by deindesign on 21.12.15.
  */
 public interface Methods {
-    Logger LOG= LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+    /**
+     * A logger.
+     */
+    Logger LOG = LoggerFactory.getLogger(Methods.class);
 
-    static Number parseNumber(String s){
-        Number rc=null;
-        if(!isBlank(s)){
+    static Number parseNumber(String s) {
+        Number rc = null;
+        if (!isBlank(s)) {
             try {
                 Double d = Double.valueOf(s);
-                if(d.longValue()==d){
-                    rc=Long.valueOf(s);
-                } else if(d.intValue()==d){
-                    rc=Integer.valueOf(s);
+                if (d.longValue() == d) {
+                    rc = Long.valueOf(s);
+                } else if (d.intValue() == d) {
+                    rc = Integer.valueOf(s);
                 } else {
                     rc = d;
                 }
-            } catch(NumberFormatException e){
+            } catch (NumberFormatException e) {
                 //silently ignored
             }
         }
         return rc;
     }
+
     /**
      * Returns the first element from a given collection, or the default value.
-     * @param c the collection
+     *
+     * @param c   the collection
      * @param def the default value
      * @param <E> the type
      * @param <V> the value type
      * @return the first element of the collection, or null
      */
-    static <E,V> E getNullOrElementFromCollection(Collection<E> c, E def){
-        E rc=def;
-        if(c!=null){
-            rc=c.iterator().next();
+    static <E, V> E getNullOrElementFromCollection(Collection<E> c, E def) {
+        E rc = def;
+        if (c != null) {
+            rc = c.iterator().next();
         }
         return rc;
     }
 
     /**
      * Returns the first non-null element from the given sequence of items.
-     * @param e the items
+     *
+     * @param e   the items
      * @param <E> the type
      * @return the first non-null element, or null
      */
-    static <E> E getFirstNonNullElement(E...e){
-        E rc=null;
-        for(E eItem : e){
-            if(eItem!=null){
-                rc=eItem;
+    static <E> E getFirstNonNullElement(E... e) {
+        E rc = null;
+        for (E eItem : e) {
+            if (eItem != null) {
+                rc = eItem;
                 break;
             }
         }
         return rc;
     }
 
-    static <E extends Enum<E>> E findEnumValueByName(Class<E> enumClass, String name, E defValue){
-        E rc=defValue;
+    static <E extends Enum<E>> E findEnumValueByName(Class<E> enumClass, String name, E defValue) {
+        E rc = defValue;
         try {
             rc = Enum.valueOf(enumClass, name);
-        } catch(IllegalArgumentException | NullPointerException e){
+        } catch (IllegalArgumentException | NullPointerException e) {
             //ok, maybe lowercase/uppercase problem
-            for(E e1 : enumClass.getEnumConstants()){
-                if(e1.name().equalsIgnoreCase(name)){
-                    rc=e1;
+            for (E e1 : enumClass.getEnumConstants()) {
+                if (e1.name().equalsIgnoreCase(name)) {
+                    rc = e1;
                     break;
                 }
             }
@@ -96,21 +101,22 @@ public interface Methods {
 
     /**
      * Finds a given enum value inside an enum.
+     *
      * @param enumClass the enum clas to check
-     * @param name the enum value name to look for
-     * @param <E> the enum type
+     * @param name      the enum value name to look for
+     * @param <E>       the enum type
      * @return the found enum value, or null if not found
      */
-    static <E extends Enum<E>> E findEnumValueByName(Class<E> enumClass, String name){
+    static <E extends Enum<E>> E findEnumValueByName(Class<E> enumClass, String name) {
         LOG.debug("Checking for enum value {} in {}", name, enumClass);
-        E rc=null;
+        E rc = null;
         try {
             rc = Enum.valueOf(enumClass, name);
-        } catch(IllegalArgumentException | NullPointerException e){
+        } catch (IllegalArgumentException | NullPointerException e) {
             //ok, maybe lowercase/uppercase problem
-            for(E e1 : enumClass.getEnumConstants()){
-                if(e1.name().equalsIgnoreCase(name)){
-                    rc=e1;
+            for (E e1 : enumClass.getEnumConstants()) {
+                if (e1.name().equalsIgnoreCase(name)) {
+                    rc = e1;
                     break;
                 }
             }
@@ -121,21 +127,24 @@ public interface Methods {
 
     /**
      * Checks if the given string contains some value.
+     *
      * @param s the string to check
      * @return TRUE if not blank, otherwise FALSE.
      * @deprecated Better use {@link #isBlank(String)} instead.
      */
     @Deprecated
-    static boolean isNotBlank(String s){
-        return s!=null&&s.trim().length()>0;
+    static boolean isNotBlank(String s) {
+        return s != null && s.trim().length() > 0;
     }
+
     /**
      * Checks if the given string contains some value.
+     *
      * @param s the string to check
      * @return TRUE if not blank, otherwise FALSE.
      */
-    static boolean isBlank(String s){
-        return s==null||s.trim().length()<=0;
+    static boolean isBlank(String s) {
+        return s == null || s.trim().length() <= 0;
     }
 
 }
