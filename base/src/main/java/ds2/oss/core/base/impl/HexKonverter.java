644fcfc809f1e7f35c957423b801d86ca3371826
Dirk Strauss <dstrauss@ds-2.de>
2020-08-24 08:41:38 +0200

    CORE-000: code format

--- base/src/main/java/ds2/oss/core/base/impl/HexKonverter.java
+++ base/src/main/java/ds2/oss/core/base/impl/HexKonverter.java
@@ -15,17 +15,16 @@
  */
 package ds2.oss.core.base.impl;
 
+import ds2.oss.core.api.HexCodec;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.enterprise.context.ApplicationScoped;
 import java.io.ByteArrayOutputStream;
 import java.lang.invoke.MethodHandles;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
-
-import javax.enterprise.context.ApplicationScoped;
-
-import ds2.oss.core.api.HexCodec;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * A hex konverter.
@@ -35,33 +34,32 @@ import org.slf4j.LoggerFactory;
  */
 @ApplicationScoped
 public class HexKonverter implements HexCodec {
-    private static final Logger LOG= LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+    private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
     /**
      * The list of hex chars.
      */
-    private static final char[] LISTE = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e',
-        'f', };
-    private boolean useSimple=true;
+    private static final char[] LISTE = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e',
+            'f',};
+    private boolean useSimple = true;
 
     /**
      * Returns the pairs of the given char sequence.
      *
-     * @param s
-     *            the char sequence
+     * @param s the char sequence
      * @return a list of pairs
      */
     private static List<String> getPairs(final char[] s) {
         final List<String> rc = new ArrayList<>();
-        String pair = "";
+        StringBuilder pair = new StringBuilder();
         String givenStr = new String(s);
         if (givenStr.toLowerCase().startsWith("0x")) {
             givenStr = givenStr.substring(2);
         }
         for (char c : givenStr.toCharArray()) {
-            pair += c;
+            pair.append(c);
             if (pair.length() == 2) {
-                rc.add(pair);
-                pair = "";
+                rc.add(pair.toString());
+                pair = new StringBuilder();
             }
         }
         return rc;
@@ -70,8 +68,7 @@ public class HexKonverter implements HexCodec {
     /**
      * Returns the alphabet position of the given char in the hex alphabet.
      *
-     * @param c
-     *            the char to find
+     * @param c the char to find
      * @return the position index, or -1 if not found
      */
     private static int getPos(final char c) {
@@ -123,8 +120,8 @@ public class HexKonverter implements HexCodec {
             return null;
         }
         String rc;
-        if(useSimple){
-            rc=new BigInteger(1, b).toString(16);
+        if (useSimple) {
+            rc = new BigInteger(1, b).toString(16);
         } else {
             final StringBuilder sb = new StringBuilder();
             for (byte b2 : b) {
@@ -133,10 +130,10 @@ public class HexKonverter implements HexCodec {
                 final int lower = b2 & 0x0f;
                 sb.append(LISTE[upper]).append(LISTE[lower]);
             }
-            rc=sb.toString();
+            rc = sb.toString();
         }
-        if(rc.length()>0&&rc.length()%2!=0){
-            rc="0"+rc;
+        if (rc.length() > 0 && rc.length() % 2 != 0) {
+            rc = "0" + rc;
         }
         return rc;
     }
