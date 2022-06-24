729d1f2492af95e5b66ca04317ed3754462844dc
Dirk Strauss <472167-dstrauss@users.noreply.gitlab.com>
2022-06-24 12:31:09 +0200

    ⬇️ downgrade jaxb-api to 2.3

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -13,7 +13,7 @@ dependencies {
     annotationProcessor("org.projectlombok:lombok:1.18.22")
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.6.0")
     compileOnly("org.projectlombok:lombok:1.18.22")
-    implementation("javax.xml.bind:jaxb-api:2.4.0")
+    implementation("javax.xml.bind:jaxb-api:2.3.1")
     compileOnly("jakarta.platform", "jakarta.jakartaee-api", "8.0.0")
     testImplementation("jakarta.platform", "jakarta.jakartaee-api", "8.0.0")
     testImplementation("org.testng:testng:7.4.0")
